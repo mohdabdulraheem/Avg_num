0a308382dfee4a3b97bcd61617e0740e2cae54e1
MOHD ABDUL <RAHEEM.MOHDABDUL@thomsonreuters.com>
2018-10-03 20:52:55 +0530

    Add project files.

---
+++ Avg_num/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Avg_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
